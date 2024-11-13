a2117d03e27e1244d7b493b84c2417bac6f739da
Schnedi3 <schnedi3@gmail.com>
2024-11-13 13:05:27 +0100

    Implement material top bars navigator

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,46 @@
 import { StyleSheet, Text, View } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { lightColors } from "@/src/constants/Colors";
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function Home() {
   return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: lightColors.accent,
+          tabBarInactiveTintColor: lightColors.disabled,
+          tabBarIndicatorStyle: { backgroundColor: lightColors.accent },
+        }}
+      >
+        <Tab.Screen name="suggested" component={SuggestedScreen} />
+        <Tab.Screen name="liked" component={LikedScreen} />
+        <Tab.Screen name="library" component={LibraryScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+function SuggestedScreen() {
+  return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Suggested</Text>
+    </View>
+  );
+}
+function LikedScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Liked</Text>
+    </View>
+  );
+}
+function LibraryScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Library</Text>
     </View>
   );
 }
