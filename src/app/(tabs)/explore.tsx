7bcbb33c7257fc1d40586e70e3a208541c392e2a
Schnedi3 <schnedi3@gmail.com>
2024-11-17 16:50:11 +0100

    Change explore and home tabs

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,139 +1,77 @@
-import { useState } from "react";
-import {
-  Dimensions,
-  Image,
-  Pressable,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from "react-native";
-import Animated, {
-  interpolate,
-  useAnimatedRef,
-  useAnimatedStyle,
-  useScrollViewOffset,
-} from "react-native-reanimated";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { AntDesign } from "@expo/vector-icons";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
-import { Walls } from "@/src/constants/Walls";
-import Carousel from "@/src/components/Carousel";
-import Sheet from "@/src/components/Sheet";
 import Colors from "@/src/constants/Colors";
 
-const { width } = Dimensions.get("window");
-const IMG_HEIGHT = 300;
+const Tab = createMaterialTopTabNavigator();
 
 export default function Explore() {
-  const [currentWall, setCurrentWall] = useState<string>("");
-  const [openSheet, setOpenSheet] = useState<boolean>(false);
-  const scrollRef = useAnimatedRef<Animated.ScrollView>();
-  const scrollOffset = useScrollViewOffset(scrollRef);
   const colorTheme = useColorScheme();
   const color = Colors[colorTheme ?? "light"];
 
-  const imageAnimatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateY: interpolate(
-            scrollOffset.value,
-            [-IMG_HEIGHT, 0, IMG_HEIGHT],
-            [-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
-          ),
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: color.secondaryBg,
         },
-        {
-          scale: interpolate(
-            scrollOffset.value,
-            [-IMG_HEIGHT, 0, IMG_HEIGHT],
-            [2, 1, 1.2]
-          ),
+        tabBarActiveTintColor: color.accent,
+        tabBarInactiveTintColor: color.disabled,
+        tabBarIndicatorStyle: {
+          backgroundColor: color.accent,
         },
-      ],
-    };
-  });
-
-  const handleWallPress = (wall: string) => {
-    setCurrentWall(wall);
-    setOpenSheet(true);
-  };
+      }}
+    >
+      <Tab.Screen name="suggested" component={SuggestedScreen} />
+      <Tab.Screen name="liked" component={LikedScreen} />
+      <Tab.Screen name="library" component={LibraryScreen} />
+    </Tab.Navigator>
+  );
+}
+function SuggestedScreen() {
+  const colorTheme = useColorScheme();
+  const color = Colors[colorTheme ?? "light"];
 
   return (
-    <GestureHandlerRootView style={styles.container}>
-      <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
-        <Carousel style={imageAnimatedStyle} />
+    <View style={[styles.container, { backgroundColor: color.primaryBg }]}>
+      <Text style={[styles.title, { color: color.primaryText }]}>
+        Suggested
+      </Text>
+    </View>
+  );
+}
 
-        <View style={[styles.walls, { backgroundColor: color.primaryBg }]}>
-          {Walls.map((wall) => (
-            <View
-              key={wall.id}
-              style={[
-                styles.wallContainer,
-                { backgroundColor: color.primaryBg },
-              ]}
-            >
-              <Pressable onPress={() => handleWallPress(wall.url)}>
-                <Image style={styles.wall} source={{ uri: wall.url }} />
-              </Pressable>
+function LikedScreen() {
+  const colorTheme = useColorScheme();
+  const color = Colors[colorTheme ?? "light"];
 
-              <View style={styles.wallOverlay}>
-                <Text
-                  style={[styles.wallTitle, { color: color.secondaryText }]}
-                >
-                  {wall.title}
-                </Text>
-                <AntDesign
-                  size={22}
-                  name="hearto"
-                  color={color.secondaryText}
-                />
-              </View>
-            </View>
-          ))}
-        </View>
-      </Animated.ScrollView>
+  return (
+    <View style={[styles.container, { backgroundColor: color.primaryBg }]}>
+      <Text style={[styles.title, { color: color.primaryText }]}>Liked</Text>
+    </View>
+  );
+}
 
-      {openSheet && (
-        <Sheet currentWall={currentWall} onClose={() => setOpenSheet(false)} />
-      )}
-    </GestureHandlerRootView>
+function LibraryScreen() {
+  const colorTheme = useColorScheme();
+  const color = Colors[colorTheme ?? "light"];
+
+  return (
+    <View style={[styles.container, { backgroundColor: color.primaryBg }]}>
+      <Text style={[styles.title, { color: color.primaryText }]}>Library</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  walls: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-    gap: 15,
-    padding: 20,
-  },
-  wallContainer: {
-    width: width / 2 - 30,
-    height: IMG_HEIGHT * 1.1,
-    borderRadius: 20,
-    overflow: "hidden",
-  },
-  wall: {
-    width: "100%",
-    height: "100%",
-  },
-  wallOverlay: {
-    width: "100%",
-    position: "absolute",
-    bottom: 0,
-    paddingHorizontal: 10,
-    paddingVertical: 15,
-    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-around",
-    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    justifyContent: "center",
   },
-  wallTitle: {
-    textAlign: "center",
+  title: {
+    fontSize: 60,
+    fontWeight: "bold",
+    opacity: 0.4,
   },
 });
