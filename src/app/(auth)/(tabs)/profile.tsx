c886389d3aa8722518fa37eccebe16d01f2c186e
Schnedi3 <schnedi3@gmail.com>
2024-12-20 19:23:02 +0100

    Improve profile styles

--- src/app/(auth)/(tabs)/profile.tsx
+++ src/app/(auth)/(tabs)/profile.tsx
@@ -1,5 +1,6 @@
 import {
   Appearance,
+  Dimensions,
   Image,
   StyleSheet,
   Text,
@@ -11,6 +12,8 @@ import { Ionicons } from "@expo/vector-icons";
 
 import { useThemeColor } from "@/src/hooks/useThemeColor";
 
+const { width } = Dimensions.get("window");
+
 export default function Profile() {
   const { color } = useThemeColor();
 
@@ -18,38 +21,41 @@ export default function Profile() {
   const { signOut } = useAuth();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        gap: 40,
-        paddingVertical: 70,
-        paddingHorizontal: 40,
-        backgroundColor: color.secondaryBg,
-      }}
-    >
+    <View style={[styles.container, { backgroundColor: color.secondaryBg }]}>
       <View style={{ alignItems: "center" }}>
         <Image source={{ uri: user?.imageUrl }} style={styles.avatar} />
         <Text style={[styles.name, { color: color.primaryText }]}>
           {user?.firstName}
         </Text>
-        <Text style={[styles.email, { color: color.disabled }]}>
+        <Text style={[styles.email, { color: color.secondaryText }]}>
           {user?.emailAddresses[0].emailAddress}
         </Text>
       </View>
 
-      <TouchableOpacity
-        activeOpacity={0.5}
-        onPress={() => signOut()}
-        style={[styles.logoutBtn, { borderColor: color.primaryText }]}
-      >
-        <Ionicons
-          name="log-out-outline"
-          style={{ fontSize: 30, color: color.primaryText }}
-        />
-        <Text style={[styles.logoutText, { color: color.primaryText }]}>
-          Log Out
+      <View style={{ gap: 10 }}>
+        <Text style={[styles.themeTitle, { color: color.primaryText }]}>
+          User
         </Text>
-      </TouchableOpacity>
+
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={[
+            styles.button,
+            { width: width / 3 - 30, borderColor: color.secondaryText },
+          ]}
+          onPress={() => signOut()}
+        >
+          <Ionicons
+            name="log-out-outline"
+            style={{ fontSize: 24, color: color.primaryText }}
+          />
+          <Text
+            style={{ fontFamily: "QuicksandMed", color: color.primaryText }}
+          >
+            Log Out
+          </Text>
+        </TouchableOpacity>
+      </View>
 
       <View style={{ gap: 10 }}>
         <Text style={[styles.themeTitle, { color: color.primaryText }]}>
@@ -92,7 +98,7 @@ export const ThemeButton = ({
   return (
     <TouchableOpacity
       activeOpacity={0.5}
-      style={[styles.themeBtn, { borderColor: color.primaryText }]}
+      style={[styles.button, { flex: 1, borderColor: color.secondaryText }]}
       onPress={() => Appearance.setColorScheme(colorScheme)}
     >
       <Ionicons
@@ -107,6 +113,12 @@ export const ThemeButton = ({
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 30,
+    paddingVertical: 70,
+    paddingHorizontal: 40,
+  },
   avatar: {
     width: 150,
     height: 150,
@@ -120,9 +132,10 @@ const styles = StyleSheet.create({
     fontFamily: "QuicksandMed",
     fontSize: 15,
   },
-  logoutBtn: {
-    width: "100%",
-    paddingVertical: 14,
+  button: {
+    width: width / 3 - 30,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
@@ -138,16 +151,5 @@ const styles = StyleSheet.create({
   themeTitle: {
     fontFamily: "QuicksandBold",
     fontSize: 20,
-  },
-  themeBtn: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 15,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderWidth: 1,
-    borderRadius: 30,
   },
 });
